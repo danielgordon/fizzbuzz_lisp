713c9db245231058023a1a011ac10331a0f113b6
Daniel Gordon <danielgordon86@gmail.com>
2013-02-21 03:41:26 -0500

    Added the file

---
+++ fizzbuzz.lisp
@@ -0,0 +1,16 @@
+(defun fizz (n)
+  (if (= 0 (mod n 3)) "fizz" ""))
+
+(defun buzz (n)
+  (if (= 0 (mod n 5)) "buzz" ""))
+
+(defun fizzbuzz (n)
+  (let 
+      ((fb-string (concatenate 'string (fizz n) (buzz n))))
+    (if (string-equal fb-string "")
+	n
+	fb-string)))
+
+(defun fizzbuzz-problem ()
+  (format t "~{~a~^~% ~}"
+	  (mapcar #'fizzbuzz (loop for i from 1 to 100 collect i))))
